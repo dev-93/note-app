e3c66aa7a172a435699af31320f2330554f72a62
kimtaenam <cjb940103@gmail.com>
2022-03-06 00:04:30 +0900

    코드 스플리팅 적용

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,11 @@ import { createWebHistory, createRouter } from "vue-router";
 const routes = [
   {
     path: "/login",
-    component: () => import("@/views/LoginPage"),
+    component: () => import("@/views/LoginPage.vue"),
   },
   {
     path: "/signUp",
-    component: () => import("@/views/SignUpPage"),
+    component: () => import("@/views/SignUpPage.vue"),
   },
 ];
 
