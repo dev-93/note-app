e43312c1f6a2ba2f508277ea4e4fc7e02f306c3e
kimtaenam <cjb940103@gmail.com>
2022-03-06 00:04:39 +0900

    router settings 2

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <router-view />
+  <div>
+    <header>
+      <router-link to="/login">로그인</router-link> |
+      <router-link to="/signup">회원가입</router-link>
+    </header>
+    <router-view />
+  </div>
 </template>
 
 <script>
@@ -10,13 +15,4 @@ export default {
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
