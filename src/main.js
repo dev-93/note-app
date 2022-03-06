98499bacbbdc53208fadbac7800a2c1fb4d50f7f
kimtaenam <cjb940103@gmail.com>
2022-03-07 02:26:30 +0900

    vuex settings

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "@/routes/index";
+import store from "@/store/index";
 
 const app = createApp(App);
+app.use(store);
 app.use(router);
 app.mount("#app");
+
+console.log(store);
