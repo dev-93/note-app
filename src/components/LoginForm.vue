1fd618d9d3668640ff87c59e0378754a2b0f0310
kimtaenam <cjb940103@gmail.com>
2022-03-06 14:55:54 +0900

    LoginForm 생성 및  마크업

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,17 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <div>
+      <label for="username">id:</label>
+      <input id="username" type="text" v-model="username" />
+    </div>
+    <div>
+      <label for="password">pw:</label>
+      <input id="password" type="text" v-model="password" />
+    </div>
+    <button>로그인</button>
+  </form>
+</template>
+
+<script></script>
+
+<style></style>
