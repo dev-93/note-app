0646d9a39324c97314bb27ba2dc98179c670fb85
Kim Taenam <cjb940103@gmail.com>
2022-03-10 13:15:20 +0900

    post api 추가

--- src/api/posts.js
+++ src/api/posts.js
@@ -12,4 +12,12 @@ function deletePost(postId) {
   return posts.delete(postId);
 }
 
-export { fetchPosts, createPost, deletePost };
+function editPost(postId, postData) {
+  return posts.put(postId, postData);
+}
+
+function fetchPost(postId) {
+  return posts.get(postId);
+}
+
+export { fetchPosts, createPost, editPost, deletePost, fetchPost };
