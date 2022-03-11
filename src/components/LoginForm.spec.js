0885320989d3c5860a46e0faf6bfd9b6f36ffb9f
Kim Taenam <cjb940103@gmail.com>
2022-03-11 14:10:59 +0900

    이메일 유효성 검사 기능 테스트

--- src/components/LoginForm.spec.js
+++ src/components/LoginForm.spec.js
@@ -3,7 +3,15 @@ import LoginForm from "./LoginForm.vue";
 
 describe("LoginForm.vue", () => {
   test("컴포넌트가 마운팅되면 렌더링", () => {
-    const wrapper = shallowMount(LoginForm);
-    expect(wrapper.vm.username).toBe("");
+    const wrapper = shallowMount(LoginForm, {
+      data() {
+        return {
+          username: "test@abc.com",
+        };
+      },
+    });
+    const idInput = wrapper.find("#username");
+    console.log("인풋의 값", idInput.element.value);
+    console.log(wrapper.vm.isUsernameValid);
   });
 });
