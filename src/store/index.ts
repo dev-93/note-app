869279c5cf9c165159ff1b80650abb01333ab10d
kimtaenam <cjb940103@gmail.com>
2022-04-17 20:25:06 +0900

    store에 state 추가

--- src/store/index.ts
+++ src/store/index.ts
@@ -11,6 +11,7 @@ const store = createStore({
   state: {
     username: getUserFromCookie() || "",
     token: getAuthFromCookie() || "",
+    postContents: "",
   },
   getters: {
     isLogin(state) {
