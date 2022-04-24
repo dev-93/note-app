2349b51ae945bb350f9f6ba62a22dc6a5ca5b87d
kimtaenam <cjb940103@gmail.com>
2022-04-24 15:01:03 +0900

    fail => success 변경

--- src/components/Greeting.spec.js
+++ src/components/Greeting.spec.js
@@ -5,6 +5,6 @@ describe("Greeting.vue", () => {
   it("greeting을 렌더한다", () => {
     const wrapper = mount(Greeting);
 
-    expect(wrapper.html().includes("Taenam")).toBe(false);
+    expect(wrapper.html().includes("Taenam")).toBe(true);
   });
 });
